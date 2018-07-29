cff92c96d4c2a4cefe989f7382b45585307a822c
Vlad Stepanenko <vladyslav.stepanenko@gmail.com>
2018-07-29 22:45:05 +0300

    Separate server.js

--- bin/server.js
+++ bin/server.js
@@ -1,23 +1,12 @@
 const http = require('http');
-const express = require('express')
+const app = require('../src/app');
 
 const hostname = "0.0.0.0";
 const port = normalizePort(process.env.PORT || '3000');
 
-const app = express();
 app.set('port', port);
 
 const server = http.createServer(app);
-const router = express.Router();
-
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        status:"ok",
-        message:"Hello"
-    });
-});
-
-app.use('/', route);
 
 server.listen(port, hostname);
 console.log(`Running on http://${hostname}:${port}`);
