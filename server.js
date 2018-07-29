c6cd09bedaca1dd71d544d8525d3448a9c049797
Vlad Stepanenko <vladyslav.stepanenko@gmail.com>
2018-07-29 22:04:11 +0300

    Add normalizePort function

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const express = require('express')
 
 const hostname = "0.0.0.0";
-const port = "3000";
+const port = normalizePort(process.env.PORT || '3000');
 
 const app = express();
 app.set('port', port);
@@ -20,3 +20,18 @@ const route = router.get('/', (req, res, next) => {
 app.use('/', route);
 
 server.listen(port, hostname);
+console.log(`Running on http://${hostname}:${port}`);
+
+function normalizePort(val) {
+    const port = parseInt(val, 10);
+    
+    if(isNaN(port)) {
+        return val;
+    }
+
+    if(port >= 0) {
+        return val;
+    }
+
+    return false;
+}
