cda8760e5df8a6e7f56d4fc20bd19bef70d9db71
Vlad Stepanenko <vladyslav.stepanenko@gmail.com>
2018-07-29 23:13:47 +0300

    Add CRUD operations

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const route = router.get('/', (req, res, next) => {
     res.status(200).send({
@@ -12,6 +16,29 @@ const route = router.get('/', (req, res, next) => {
     });
 });
 
+const create = router.post('/', (req, res, next) => {
+    res.status(201).send({
+        status:"saved"
+    });
+});
+
+const edit = router.put('/:id', (req, res, next) => {
+    const id = req.params.id;
+    res.status(201).send({
+        status:"edited"
+    });
+});
+
+const remove = router.delete('/:id', (req, res, next) => {
+    const id = req.params.id;
+    res.status(200).send({
+        status:"deleted"
+    });
+});
+
 app.use('/', route);
+app.use('/expenses', create);
+app.use('/expenses', edit);
+app.use('/expenses', remove);
 
 module.exports = app;
