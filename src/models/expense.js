b34b0eff8813f20a35e70b137d89ed37536db0a5
Vlad Stepanenko <vladyslav.stepanenko@gmail.com>
2018-07-30 13:40:25 +0300

    Add Expense model

---
+++ src/models/expense.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+    tag: {
+        type: String,
+        required: true
+    },
+    merchantName: {
+        type: String,
+        required: true
+    },
+    total: {
+        type: Number,
+        required: true
+    },
+    time: {
+        type: Date,
+        default: Date.now
+    },
+    paymentType: {
+        type: String,
+        required: true,
+        enum: ['Cash', 'Credit Card'],
+    },
+    photoUrl: {
+        type: String
+    },
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Expense', schema);
