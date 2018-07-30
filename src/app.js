63820c4ad619607178f566fa4b9b4a66d65bc2fc
Vlad Stepanenko <vladyslav.stepanenko@gmail.com>
2018-07-30 23:21:12 +0300

    Add user route

--- src/app.js
+++ src/app.js
@@ -14,11 +14,13 @@ const Expense = require('./models/expense');
 const User = require('./models/user');
 
 // Routers
-const expenseRoute = require('../src/routes/expense-route');
+const expenseRoute = require('./routes/expense-route');
+const userRoute = require('./routes/user-route');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/expenses', expenseRoute);
+app.use('/users', userRoute);
 
 module.exports = app;
