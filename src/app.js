15765b4df7769f57ed7c586befd4eae30cd8eaa5
Vlad Stepanenko <vladyslav.stepanenko@gmail.com>
2018-07-30 13:07:03 +0300

    Add mongoose

--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
 
 // Routers
 const expenseRoute = require('../src/routes/expense-route');
 
-const app = express();
+mongoose.connect('mongodb://192.168.43.218:27017/expenses');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
